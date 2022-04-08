4ec448b822a8bb63f15caab1967dbd48cbc6ea3e
ilikerb0407 <kevin12342247@gmail.com>
2022-04-08 18:32:40 +0800

    Created Journey ViewController

---
+++ RideTogether/RideTogether/JourneyViewController.swift
@@ -0,0 +1,19 @@
+//
+//  JourneyViewController.swift
+//  RideTogether
+//
+//  Created by Kai Fu Jhuang on 2022/4/8.
+//
+
+import UIKit
+
+class JourneyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
