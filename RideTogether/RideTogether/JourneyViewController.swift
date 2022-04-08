760cbc7967d5e7677be7ef6e76e992b2802affa1
ilikerb0407 <kevin12342247@gmail.com>
2022-04-08 20:04:48 +0800

    BaseViewController is created

--- RideTogether/RideTogether/JourneyViewController.swift
+++ RideTogether/RideTogether/JourneyViewController.swift
@@ -6,9 +6,18 @@
 //
 
 import UIKit
+import MapKit
+import CoreGPX
+import CoreLocation
+import Firebase
+import Lottie
+
 
 class JourneyViewController: UIViewController {
 
+    
+    weak var map: GPXMap!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
