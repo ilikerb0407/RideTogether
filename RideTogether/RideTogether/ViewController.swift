cca29fd38a9802e8c4b66b9d1f41d478a3d156d2
ilikerb0407 <kevin12342247@gmail.com>
2022-04-08 20:04:48 +0800

    BaseViewController is created

--- RideTogether/RideTogether/ViewController.swift
+++ RideTogether/RideTogether/ViewController.swift
@@ -6,12 +6,13 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
