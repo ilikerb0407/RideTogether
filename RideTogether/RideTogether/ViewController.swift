0d23e3cafef84ef6d21ce9b1d88b71f69a07726d
ilikerb0407 <kevin12342247@gmail.com>
2022-04-08 17:13:52 +0800

    Build project - RideTogether

---
+++ RideTogether/RideTogether/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RideTogether
+//
+//  Created by Kai Fu Jhuang on 2022/4/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
