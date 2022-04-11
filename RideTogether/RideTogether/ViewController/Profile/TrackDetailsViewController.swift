418c8a47f335807d29ec5014a02046d3eb704cf2
ilikerb0407 <kevin12342247@gmail.com>
2022-04-12 01:29:17 +0800

    Create TrackDetail ViewController

--- RideTogether/RideTogether/ViewController/Profile/TrackDetailsViewController.swift
+++ RideTogether/RideTogether/ViewController/Profile/TrackDetailsViewController.swift
@@ -6,16 +6,60 @@
 //
 
 import UIKit
+import SwiftUI
+import MapKit
+import Firebase
+import CoreGPX
+import CoreLocation
 
 class TrackDetailsViewController: UIViewController {
 
     
+    @IBOutlet weak var map: GPXMapView!
     
-//    @IBOutlet weak var mapInDetail: MKMapView!
+    private let mapViewDelegate = MapViewDelegate()
+    
+    // 只會有一筆
+    var record = Record()
+    
+    
+    
+    func setUp() {
+        
+        navigationController?.isNavigationBarHidden = true
+        
+        map.delegate = mapViewDelegate
+        
+        view.addSubview(map)
+        
+        tabBarController?.tabBar.isHidden = true
+        
+        backButton()
+        
+//        praseGPXFile()
+        
+    }
+    
+    func backButton() {
+        let button = PreviousPageButton(frame: CGRect(x: 30, y: 30, width: 50, height: 50))
+        button.addTarget(self, action: #selector(popToPreviosPage), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    
+    @objc func popToPreviosPage(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    func praseGPXFile() {
+        let url = URL(string: record.recordRef)
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setUp()
     }
     
 
