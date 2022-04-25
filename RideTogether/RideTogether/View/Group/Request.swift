cc5352764010b26ab99478926b5a5507a1b13b55
ilikerb0407 <kevin12342247@gmail.com>
2022-04-25 16:56:31 +0800

    create Request TableViewCell

---
+++ RideTogether/RideTogether/View/Group/Request.swift
@@ -0,0 +1,23 @@
+//
+//  Request.swift
+//  RideTogether
+//
+//  Created by Kai Fu Jhuang on 2022/4/25.
+//
+
+import UIKit
+
+class Request: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
