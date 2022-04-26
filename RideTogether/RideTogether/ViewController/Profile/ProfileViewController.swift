024a26cb8ef0c120235a7083f7796fee4080edff
ilikerb0407 <kevin12342247@gmail.com>
2022-04-26 20:28:04 +0800

    fix cancel account

--- RideTogether/RideTogether/ViewController/Profile/ProfileViewController.swift
+++ RideTogether/RideTogether/ViewController/Profile/ProfileViewController.swift
@@ -77,7 +77,7 @@ extension ProfileViewController : UITableViewDelegate {
                 self.signOut()
             }
             let removeAccount = UIAlertAction(title: AccountActionSheet.allCases[1].rawValue, style: .destructive) { _ in
-        
+                self.deleteAccount()
                 self.showAlertAction(title: "刪除帳號", message: "請來信至kevin12342247@gmail.com")
             }
             let cancel = UIAlertAction(title: AccountActionSheet.allCases[2].rawValue, style: .cancel) { _ in }
