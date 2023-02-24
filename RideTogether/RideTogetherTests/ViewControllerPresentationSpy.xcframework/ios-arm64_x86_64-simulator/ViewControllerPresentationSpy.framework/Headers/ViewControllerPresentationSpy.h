55a76d2feeb54c36056464dda637f6dec3665a63
ilikerb0407 <kevin12342247@gmail.com>
2023-02-24 16:34:08 +0800

    Add Frame - ViewControllerPresentationSpy

---
+++ RideTogether/RideTogetherTests/ViewControllerPresentationSpy.xcframework/ios-arm64_x86_64-simulator/ViewControllerPresentationSpy.framework/Headers/ViewControllerPresentationSpy.h
@@ -0,0 +1,8 @@
+// ViewControllerPresentationSpy by Jon Reid, https://qualitycoding.org
+// Copyright 2023 Jonathan M. Reid. https://github.com/jonreid/ViewControllerPresentationSpy/blob/main/LICENSE.txt
+// SPDX-License-Identifier: MIT
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double ViewControllerPresentationSpyVersionNumber;
+FOUNDATION_EXPORT const unsigned char ViewControllerPresentationSpyVersionString[];
