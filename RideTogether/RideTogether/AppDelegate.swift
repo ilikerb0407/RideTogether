37b113d3fca05bd609c4e39429c907606a144a6c
KaiFu <kevin12342247@gmail.com>
2024-07-16 21:53:21 +0800

    Refractor: AppDelegate

--- RideTogether/RideTogether/AppDelegate.swift
+++ RideTogether/RideTogether/AppDelegate.swift
@@ -5,27 +5,46 @@
 //  Created by Kai Fu Jhuang on 2022/4/8.
 //
 
-import CoreData
+import UIKit
 import Firebase
 import FirebaseAuth
 import IQKeyboardManagerSwift
-import UIKit
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        print("didFinishLaunchingWithOptions")
-        FirebaseApp.configure()
-        IQKeyboardManager.shared.enable = true
 
-        if let userId = Auth.auth().currentUser {
-            print("\(userId.uid) and \(String(describing: userId.email))")
-        }
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setupFirebase()
 
-        let navigationBarAppearance = UINavigationBar.appearance()
-        navigationBarAppearance.tintColor = UIColor.B5
-        navigationBarAppearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.B5 as Any]
+        setupKeyboardManager()
+
+        setupNavigationBarAppearance()
+
+        logCurrentUser()
 
         return true
+    }
+
+    private func setupFirebase() {
+        FirebaseApp.configure()
+    }
+
+    private func setupKeyboardManager() {
+        IQKeyboardManager.shared.enable = true
+    }
+
+    private func setupNavigationBarAppearance() {
+        let navigationBarAppearance = UINavigationBar.appearance()
+        navigationBarAppearance.tintColor = UIColor.B5
+        navigationBarAppearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.B5]
+    }
+
+    private func logCurrentUser() {
+        if let currentUser = Auth.auth().currentUser {
+            print("Current User UID: \(currentUser.uid)")
+            print("Current User Email: \(currentUser.email ?? "N/A")")
+        } else {
+            print("No user currently logged in")
+        }
     }
 }
